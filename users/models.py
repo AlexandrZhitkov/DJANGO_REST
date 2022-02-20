fdb8e4859b9e0dc7000b36c9c1abdc52b91fc672
AlexGagarin93 <smackdownmaps1317@yandex.ru>
2022-02-20 21:26:06 +0300

    migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+
+    class Meta:
+        verbose_name = _('Пользователь')
+        verbose_name_plural = _('Пользователи')
